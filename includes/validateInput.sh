74310a68d47b3a871f797c0c279543a02ea93cf3
Elin <elin.falk_sorqvist@igp.uu.se>
2017-01-25 14:43:18 +0100

    Fixed wrong layout of hard parantheses

--- includes/validateInput.sh
+++ includes/validateInput.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-. $SERA_PATH/includes/errorHandling.sh
+. $SERA_PATH/includes/errorHandling.sh;
 
 # check if valid input
 if [[ -z $ROOT_PATH_INPUT ]] || [[ -z $INPUT_FILE ]]; then
@@ -9,8 +9,8 @@ elif [[ ! -d $ROOT_PATH_INPUT ]]; then
 	fileioerror $ROOT_PATH_INPUT; kill -SIGINT $$;
 elif [[ ! -e $INPUT_FILE ]]; then
 	fileioerror $INPUT_FILE; kill -SIGINT $$;
-elif [[ ! -z $INTERFACE ] && [[ "$INTERFACE" != "gui" ]]; then
-	usage; exit 1
+elif [[ ( ! -z $INTERFACE ) && ( "$INTERFACE" != "gui" ) ]]; then
+	usage; exit 1;
 # if gui set, check that dialog is found in bin folders set in system path
 elif [[ $INTERFACE && `echo $PATH | awk 'BEGIN{FS=":"}{for (i=1; i<=NF; i++) print $i}' | xargs ls | grep -w -P '^dialog$' | wc -l` = 0 ]]; then
 	settingserror "Dialog not found in system, required for graphical user interface."; kill -SIGINT $$;
