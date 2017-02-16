82ee244e4769abf5299685a62eec06516e7891b8
Elin <elin.falk_sorqvist@igp.uu.se>
2017-02-16 08:39:56 +0100

    Update the scripts to run the pipeline to simplify the running procedure

--- SERA_PBS_SUBMIT_off.sh
+++ SERA_PBS_SUBMIT_off.sh
@@ -22,31 +22,31 @@ title="SlurmSERA"
 # id brief_explanation default_status associated_script dependency_on_id run_after_samples(0: do not wait, 1: wait and run all samples, 2: wait and run once)
 steps=(
 1 "Create ampregion, ampROI, seqregion and seqROI Files" off "Create_region_files_python.sh" false 2 \
-2 "Remove adapter sequences from reads" off "Run_cutAdapt_swift_all.sh" false 0 \
-5 "Run FastQC" off "Run_FastQC.sh" "2" 0 \
-6 "Align with BWA against Genome" off "Run_Bwa.sh" "2" 0 \
-7 "Amplicon mapping" off "AmpliconMapping.sh" "6" 0 \
-10 "Create SNPseq file" off "Create_SNPseq_file.sh" "1" 2 \
-11 "Run jSNPmania" off "Run_jSNPmania.sh" "6:7:10" 0 \
-12 "SNPmania output to Annovar input" off "jSNPmania2AnnovarInput.sh" "11" 0 \
-13 "Run Annovar" off "Run_Annovar.sh" "12" 0 \
-14 "Combine Annovar output to one file" off "Combine_annovarOutput_to_one_file.sh" "13" 2 \
-20 "Run Pindel" off "Run_Pindel.sh" "6" 0 \
+2 "Create SNPseq file" off "Create_SNPseq_file.sh" "1" 2 \
+10 "Remove adapter sequences from reads" off "Run_cutAdapt_swift_all.sh" false 0 \
+11 "Run FastQC" off "Run_FastQC.sh" "10" 0 \
+12 "Align with BWA against Genome" off "Run_Bwa.sh" "10" 0 \
+13 "Amplicon mapping" off "AmpliconMapping.sh" "12" 0 \
+14 "Run jSNPmania" off "Run_jSNPmania.sh" "2:12:13" 0 \
+15 "SNPmania output to Annovar input" off "jSNPmania2AnnovarInput.sh" "14" 0 \
+16 "Run Annovar" off "Run_Annovar.sh" "15" 0 \
+17 "Combine Annovar output to one file" off "Combine_annovarOutput_to_one_file.sh" "16" 2 \
+20 "Run Pindel" off "Run_Pindel.sh" "12" 0 \
 21 "Annotate Pindel with Annovar" off "AnnotatePindel.sh" "20" 0 \
-25 "Output all mutations (hotspots and others)" off "FilterMutations.sh" "13:21" 0 \
+25 "Output all mutations (hotspots and others)" off "FilterMutations.sh" "16:21" 0 \
 26 "Convert Annovar output to vcf-format" off "ConvertAnnovaroutput2vcf.sh" "25" 0 \
-30 "Extract MSI markers" off "ExtractMSI.sh" "13:21:25" 0 \
+30 "Extract MSI markers" off "ExtractMSI.sh" "16:21:25" 0 \
 31 "Combine extracted MSI markers to one file" off "Combine_extracted_MSI.sh" "30" 2 \
-32 "Extract EGFR information" off "Extract_T790M.sh" "11" 0 \
+32 "Extract EGFR information" off "Extract_T790M.sh" "14" 0 \
 33 "Combine extracted EGFR information to one file" off "Combine_extracted_EGFR.sh" "32" 2 \
-34 "Extract SNP information" off "ExtractSNPinfo.sh" "11" 0 \
+34 "Extract SNP information" off "ExtractSNPinfo.sh" "14" 0 \
 35 "Combine extracted SNP information to one file" off "Combine_extracted_SNPs.sh" "34" 2 \
-40 "Calculate amplification ratio" off "CalculateAmplificationRatio.sh" "11" 0 \
-50 "Calculate fraction covered" off "FractionCovered.sh" "11" 0 \
-51 "Calculate mean cov per gene" off "Calculate_Gene_Cov.sh" "11" 0 \
-52 "Calculate mean cov per region (exon)" off "Calculate_Region_Cov.sh" "11" 0 \
-60 "Create hits per base file from SNPmania file SeqRegion" off "SNPmania2hitsPerBase_seqregion.sh" "11" 0 \
-61 "Create hits per base file from SNPmania file SeqRoi" off "SNPmania2hitsPerBase_seqroi.sh" "11" 0 \
+40 "Calculate amplification ratio" off "CalculateAmplificationRatio.sh" "14" 0 \
+50 "Calculate fraction covered" off "FractionCovered.sh" "14" 0 \
+51 "Calculate mean cov per gene" off "Calculate_Gene_Cov.sh" "14" 0 \
+52 "Calculate mean cov per region (exon)" off "Calculate_Region_Cov.sh" "14" 0 \
+60 "Create hits per base file from SNPmania file SeqRegion" off "SNPmania2hitsPerBase_seqregion.sh" "14" 0 \
+61 "Create hits per base file from SNPmania file SeqRoi" off "SNPmania2hitsPerBase_seqroi.sh" "14" 0 \
 62 "Produce plot-values SeqRegion Against SeqRegion" off "Create_plotValues_seqregion_against_seqregion.sh" "60" 0 \
 63 "Produce plot-values SeqRoi Against SeqRoi" off "Create_plotValues_seqroi_against_seqroi.sh" "61" 0 \
 64 "Create stenberg nusbaum input file" off "makeGnuplotInputFilePost.sh" "62:63" 2 \
@@ -56,8 +56,8 @@ steps=(
 71 "Separate on- and offtarget bases from pileup" off "PileUp2onOffTarget.sh" "70" 0 \
 72 "Extract specificities" off "ExtractSpecificity.sh" "71" 0 \
 73 "Calculate variant allele ratio per strand" off "StrandVarAlleleRatio.sh" "11" 0 \
-100 "Copy results to OUTBOX" off "Move_results2outbox.sh" "13:21:25:26:31:33:35:40" 2 \
-101 "Copy results to storage" off "Move_results2storage.sh" "13:21:25:26:31:33:35:40" 2);
+100 "Copy results to OUTBOX" off "Move_results2outbox.sh" "16:21:25:26:31:33:35:40" 2 \
+101 "Copy results to storage" off "Move_results2storage.sh" "16:21:25:26:31:33:35:40" 2);
 
 let NUMBOFSTEPS=${#steps[@]}-1;
 
