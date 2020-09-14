fb55ac24a39d1464d040c1c381ce6898b37640f5
Patrik (Eriksson) Smeds <patrik.smeds@gmail.com>
2020-09-14 08:49:57 +0200

    Update Run_Bwa.sh
    Fix typo

--- bin/bashscript/Run_Bwa.sh
+++ bin/bashscript/Run_Bwa.sh
@@ -23,7 +23,7 @@ fastq_files_r1=($(echo "$RAWDATA_PE1" | tr " " "\n"));
 if [[ -e ${ROOT_PATH}/seqdata/${SAMPLEID}.read1.fastq.gz ]]; then
 	PE1=${ROOT_PATH}/seqdata/${SAMPLEID}.read1.fastq.gz;
 else
-    if [[ ${#fastq_files_r1[@]]} > 1 ]];
+    if [[ ${#fastq_files_r1[@]} > 1 ]];
     then
         if [ -n "$(find ${ROOT_PATH}/seqdata -name ${SAMPLEID}_S*_L000_R1_001.fastq.gz | head -1)" ];
         then
@@ -38,7 +38,7 @@ fi
 if [[ -e ${ROOT_PATH}/seqdata/${SAMPLEID}.read2.fastq.gz ]]; then
 	PE2=${ROOT_PATH}/seqdata/${SAMPLEID}.read2.fastq.gz;
 else
-    if [[ ${#fastq_files_r1[@]]} > 1 ]];
+    if [[ ${#fastq_files_r1[@]} > 1 ]];
     then
         if [ -n "$(find ${ROOT_PATH}/seqdata -name ${SAMPLEID}_S*_L000_R2_001.fastq.gz | head -1)" ];
         then
