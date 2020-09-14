bce30d4ecd58917377d3bf3e684165bd5ed2f577
Patrik Smeds <patrik.smeds@gmail.com>
2020-09-14 13:44:25 +0200

    Prepare SERA to work with Marvin.

---
+++ config/modules_marvin.sh
@@ -0,0 +1,11 @@
+#!/bin/bash -l
+
+# load marvin modules
+module load samtools/1.2
+module load blast/2.2.25
+module load java-openjdk/1.7.0
+module load bioperl/1.6.1
+module load fastqc/0.11.2
+module load cutadapt/1.8.0
+module load bwa/0.7.12
+module load trimmomatic/0.32
--- includes/prepareEnvironment.sh
+++ includes/prepareEnvironment.sh
@@ -38,7 +38,8 @@ if [[ $GLOBALS = "HOME" ]]; then
 
 elif [[ $GLOBALS = "MORIARTY" ]]; then 
     . $SERA_PATH/config/globalsMoriarty.sh;
-
+elif [[ $GLOBALS = "MARVIN" ]]; then
+    . $SERA_PATH/config/globalsMarvin.sh;
 else
     . $SERA_PATH/config/globalsProj.sh;
 fi
